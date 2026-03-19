df66ded1c3633e2d562b562469982384270eb4e9
Mayank <mayank25316@gmail.com>
2026-03-19 15:57:46 +0530

    Fix: Renamed image to force Git update

--- frontend/src/landing-page/products/ProductPage.jsx
+++ frontend/src/landing-page/products/ProductPage.jsx
@@ -1,6 +1,6 @@
 import Hero from "./Hero";
 import LeftSection from "./LeftSection";
-import Kite from "../../assets/Media/Kite.png";
+import KiteLogo from "../../assets/Media/KiteLogo.png";
 import coin from "../../assets/Media/coin.png";
 import varsity from "../../assets/Media/varsity.png";
 import RightSection from "./RightSection";
@@ -12,7 +12,7 @@ export default function ProductPage(){
     return(
         <>
         <Hero/>
-        <LeftSection imgURL={Kite} productTitle="Kite" productDesc="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." tryDemo="https://kite-demo.zerodha.com/dashboard" learnMore="https://zerodha.com/products/kite" googlePlay="" appStore=""/>
+        <LeftSection imgURL={KiteLogo} productTitle="Kite" productDesc="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." tryDemo="https://kite-demo.zerodha.com/dashboard" learnMore="https://zerodha.com/products/kite" googlePlay="" appStore=""/>
         <RightSection imgURL={console} productTitle="Console" productDesc="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations." learnMore="https://zerodha.com/products/console"/>
         <LeftSection imgURL={coin} productTitle="Coin" productDesc="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices." tryDemoText="Coin" tryDemo="https://coin.zerodha.com/" learnMore="" googlePlay="" appStore=""/>
         <RightSection imgURL={Kiteconnect} productTitle="Kite Connect API" productDesc="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase." learnMore="https://zerodha.com/products/api/"/>
